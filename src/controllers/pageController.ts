398a8aa76c5ef31842c2f3b9e8581cce02eac3ef
Kinhopoa <marcosfraga05@gmail.com>
2022-07-10 03:55:30 -0300

    criação de partials e alteração no controller page

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send('home no controller')
-    // res.render('pages/page');
+    
+    res.render('pages/page');
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+    res.send('dogs')
     // res.render('pages/dogs');
 }
 
 export const cats = (req: Request, res: Response) => {
-
+    res.send('cats')
     // res.render('pages/cats');
 }
 
 export const fishes = (req: Request, res: Response) => {
-
+    res.send('fishes')
     // res.render('pages/fishes');
 }
